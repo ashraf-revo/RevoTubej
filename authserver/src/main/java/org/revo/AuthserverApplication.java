52bcf151a4868835defedcfc355c3435b0cfea50
ashraf <ashraf1abdelrasool@gmail.com>
2017-04-23 06:44:01 +0200

    add error page to uaa

--- authserver/src/main/java/org/revo/AuthserverApplication.java
+++ authserver/src/main/java/org/revo/AuthserverApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RestController;
 
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableJpaAuditing
-@EnableResourceServer
 //@EnableEurekaClient
 @EnableConfigurationProperties(Env.class)
 public class AuthserverApplication {
--- authserver/src/main/java/org/revo/Config/SecurityConfig.java
+++ authserver/src/main/java/org/revo/Config/SecurityConfig.java
@@ -6,14 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * Created by ashraf on 10/04/17.
  */
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
@@ -25,10 +26,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http
-                .authorizeRequests().antMatchers("/home", "/user").authenticated().anyRequest().permitAll()
-                .and().formLogin().loginPage("/signin").permitAll()
-                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/signout"))
-                .and().requestMatchers().antMatchers("/home", "/css/**", "/", "/done", "/active/*", "/signin", "/signup", "/signout", "/oauth/authorize", "/oauth/confirm_access", "/oauth/check_token");
+                .authorizeRequests().antMatchers("/home").authenticated().anyRequest().permitAll()
+                .and().formLogin().loginPage("/signin").permitAll().defaultSuccessUrl("/home")
+                .and().logout().logoutUrl("/signout")
+                .and().requestMatchers().antMatchers("/home", "/", "/done", "/active/*", "/signin", "/signup", "/signout", "/oauth/**");
+
     }
 
     @Bean
