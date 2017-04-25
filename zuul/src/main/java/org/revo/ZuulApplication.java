62c920fa126bbb9221c516abb42e55961ba45e65
ashraf <ashraf1abdelrasool@gmail.com>
2017-04-25 03:04:47 +0200

    tested kernal

--- zuul/src/main/java/org/revo/ZuulApplication.java
+++ zuul/src/main/java/org/revo/ZuulApplication.java
@@ -3,12 +3,13 @@ package org.revo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 //@EnableEurekaClient
@@ -22,10 +23,10 @@ public class ZuulApplication extends WebSecurityConfigurerAdapter {
 
     public void configure(HttpSecurity http) throws Exception {
         http
-                .antMatcher("/**").authorizeRequests()
+                .authorizeRequests()
+                .antMatchers("/uaa/**").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
                 .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
     }
-
 }
