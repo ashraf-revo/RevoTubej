a7ae4c4a8282d11c6148e0b779cdb00060f365c3
ashraf <ashraf1abdelrasool@gmail.com>
2017-04-30 08:56:03 +0200

    ui add  done

--- ui/src/main/java/org/revo/UiApplication.java
+++ ui/src/main/java/org/revo/UiApplication.java
@@ -2,13 +2,23 @@ package org.revo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 //@EnableEurekaClient
+@Controller
 public class UiApplication {
+    private final String p = "{path:[^.]*}";
 
-	public static void main(String[] args) {
-		SpringApplication.run(UiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UiApplication.class, args);
+    }
+
+    @GetMapping(value = {p, p + "/" + p, p + "/" + p + "/" + p})
+    public ModelAndView index() {
+        return new ModelAndView("forward:/index.html");
+    }
 }
